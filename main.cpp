b885aaa24b04376332c329fefb81041de371a6a2
msnp <mohamadnematpoor@gmail.com>
2021-01-14 19:08:06 +0330

    msnp enemy naghes

--- main.cpp
+++ main.cpp
@@ -8,11 +8,24 @@ int main()
     sf::Font vazir_font;
     sf::Text score;
 
+
             sf::Texture texture;
-        if (!texture.loadFromFile("cb.bmp")||!vazir_font.loadFromFile("Vazir.ttf"))
+        if (!texture.loadFromFile("enemy.png")||!vazir_font.loadFromFile("Vazir.ttf"))
             return -1;
-        sf::Sprite sprite(texture);
-        sprite.setPosition(0100,10);
+
+        sf::Sprite sprite[24];
+        for(int n=0;n<24;n++){
+        sprite[n].setTexture(texture);
+          int i,j;
+if (0<=n&&n<=7)
+    j=20;
+if (8<=n&&n<=15)
+    j=20+64+28*2;
+if (16<=n&&n<=23)
+    j=20+64+28*2+64+28*2;
+i=(n%8)*64+28*2;
+sprite[n].setPosition(i,j);
+        }
         score.setFont(vazir_font);
         score.setCharacterSize(15);
 
@@ -32,11 +45,22 @@ int main()
                 window.close();
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+{
+    // left key is pressed: move our character
+   // sprite.move(-1.f, 0.f);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+{
+    // left key is pressed: move our character
+//    sprite.move(+1.f, 0.f);
+}
 
 score.setString("salam");
         window.clear();
 // Draw the textured sprite
-        window.draw(sprite);
+for (int i=0;i<24;i++)
+        window.draw(sprite[i]);
         window.draw(score);
         window.display();
     }
