7146f210c4f2f0ae1df7d41c3e56134609d693fd
ali <soltanibookali1380@gmail.com>
2021-01-22 03:16:37 -0800

    ali : enemy bullet created & moved but without delay

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 #define enemy_count 30
 sf::Sprite build_bullet(sf::Sprite MyShip, sf::Sprite bullet, sf::Sprite empty_temp);
 void fire(sf::Sprite bullet);
@@ -95,12 +98,13 @@ void move_enemies(sf::Sprite s[enemy_count])
 }
 int main()
 {
+    srand(time(NULL));
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800,600),"my window");
     sf::Font vazir_font;
     sf::Text score;
-    sf::Texture bullet_texture ;
-    if(!bullet_texture.loadFromFile("images/bullet_main_3.png"))
+    sf::Texture bullet_texture , bullet_texture_enemy;
+    if(!bullet_texture.loadFromFile("images/bullet_main_3.png") || !bullet_texture_enemy.loadFromFile("images/bullet_main_3_enemy.png"))
         printf("we don't have bullets !");
     //sf::Sprite bullet(bullet_texture);
     sf::Texture texture;
@@ -139,13 +143,72 @@ int main()
     //sf::Vector2f position_MyBullet = bullet.getPosition();
     //std::cout<<bullet.getPosition().x<<bullet.getPosition().y<<"\n";
     // up here .
+
+
+
+
+    //bullet create
+
+    //bullet enemy
+    bullet_class bulletEnemy1(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy2(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy3(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy4(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy5(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy6(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy7(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy8(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy9(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    bullet_class bulletEnemy10(bullet_texture_enemy , sprite[rand()%30].getPosition());
+    //bullet MyShip
     bullet_class bullet1(bullet_texture, position_MyShip);
     bullet_class bullet2(bullet_texture, position_MyShip);
     bullet_class bullet3(bullet_texture, position_MyShip);
     while (window.isOpen())
     {
+        // bullet enemy receive pos
+        if (bulletEnemy1.allive == 0)
+        {
+            bulletEnemy1.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy2.allive == 0)
+        {
+            bulletEnemy2.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy3.allive == 0)
+        {
+            bulletEnemy3.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy4.allive == 0)
+        {
+            bulletEnemy4.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy5.allive == 0)
+        {
+            bulletEnemy5.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy6.allive == 0)
+        {
+            bulletEnemy6.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy7.allive == 0)
+        {
+            bulletEnemy7.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy8.allive == 0)
+        {
+            bulletEnemy8.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy9.allive == 0)
+        {
+            bulletEnemy9.get_pos(sprite[rand()%30].getPosition());
+        }
+        if (bulletEnemy10.allive == 0)
+        {
+            bulletEnemy10.get_pos(sprite[rand()%30].getPosition());
+        }
 
-        //receive new pos
+        //receive new pos for myship bullets
         sf::Vector2f position_MyShip = spriteMyShip.getPosition();
         if (bullet1.allive == 0)
         {
@@ -160,8 +223,6 @@ int main()
             bullet3.get_pos(position_MyShip);
         }
 
-
-
         //clos window
         sf::Event event;
         while (window.pollEvent(event))
@@ -169,6 +230,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+
+        //front
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
             // left key is pressed: move our character
@@ -197,9 +261,71 @@ int main()
             //allive_bullet = 1 ;
             //bullet bullet1( bullet_texture, position_MyShip);
         }
+        //creating enemy bullet
+        bulletEnemy1.allive = 1;
+        bulletEnemy2.allive = 1;
+        bulletEnemy3.allive = 1;
+        bulletEnemy4.allive = 1;
+        bulletEnemy5.allive = 1;
+        bulletEnemy6.allive = 1;
+        bulletEnemy7.allive = 1;
+        bulletEnemy8.allive = 1;
+        bulletEnemy9.allive = 1;
+        bulletEnemy10.allive = 1;
+
         score.setString("salam");
         window.clear();
 
+        //draw enemy bullet
+        if (bulletEnemy1.allive == 1)
+        {
+            window.draw(bulletEnemy1.bullet_sprite);
+            bulletEnemy1.move_bullet(-.1f);
+        }
+        if (bulletEnemy2.allive == 1)
+        {
+            window.draw(bulletEnemy2.bullet_sprite);
+            bulletEnemy2.move_bullet(-.2f);
+        }
+        if (bulletEnemy3.allive == 1)
+        {
+            window.draw(bulletEnemy3.bullet_sprite);
+            bulletEnemy3.move_bullet(-.3f);
+        }if (bulletEnemy4.allive == 1)
+        {
+            window.draw(bulletEnemy4.bullet_sprite);
+            bulletEnemy4.move_bullet(-.1f);
+        }
+        if (bulletEnemy5.allive == 1)
+        {
+            window.draw(bulletEnemy5.bullet_sprite);
+            bulletEnemy5.move_bullet(-.2f);
+        }
+        if (bulletEnemy6.allive == 1)
+        {
+            window.draw(bulletEnemy6.bullet_sprite);
+            bulletEnemy6.move_bullet(-.3f);
+        }if (bulletEnemy7.allive == 1)
+        {
+            window.draw(bulletEnemy7.bullet_sprite);
+            bulletEnemy7.move_bullet(-.1f);
+        }
+        if (bulletEnemy8.allive == 1)
+        {
+            window.draw(bulletEnemy8.bullet_sprite);
+            bulletEnemy8.move_bullet(-.2f);
+        }
+        if (bulletEnemy9.allive == 1)
+        {
+            window.draw(bulletEnemy9.bullet_sprite);
+            bulletEnemy9.move_bullet(-.3f);
+        }
+        if (bulletEnemy10.allive == 1)
+        {
+            window.draw(bulletEnemy10.bullet_sprite);
+            bulletEnemy10.move_bullet(-.3f);
+        }
+        //draw MyShip bullet
         if (bullet1.allive == 1)
         {
             window.draw(bullet1.bullet_sprite);
@@ -208,7 +334,6 @@ int main()
         if (bullet2.allive == 1)
         {
             window.draw(bullet2.bullet_sprite);
-            //std::cout<<"hello !";
             bullet2.move_bullet(.2f);
         }
         if (bullet3.allive == 1)
@@ -223,7 +348,7 @@ int main()
         //  bullet.move(0.f, -.1f);
         //}
         move_enemies(sprite);
-// Draw the textured sprite
+        // Draw the textured sprite
         for (int i=0; i<enemy_count; i++)
             window.draw(sprite[i]);
         window.draw(spriteMyShip);
